dcfb331a04ffefcbd1752657c15947bef4c32c98
Hammed Akinyemi <hakra911@gmail.com>
2021-07-04 20:45:03 +0100

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e1bcd6391b774597caee23351bb33c93'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd44469b1-c06a-4b19-8c97-c7364c690b73'
+};
